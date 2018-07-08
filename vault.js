156b7603bc5db4faaa7ef44c579475d00bc9caa8
Sarah <yamashige.sarah@gmail.com>
2018-07-07 16:11:11 -1000

    added in alternate ways of writing the getValue

--- vault.js
+++ vault.js
@@ -4,6 +4,8 @@ module.exports = function() {
     const setValue = function (key, value) {
         secretVault[key] = value;
     }
+    
+    //One way of writing it
     const getValue = function (key) {
         if (secretVault[key] === undefined) {
             return null;
@@ -11,6 +13,25 @@ module.exports = function() {
             return secretVault[key];
         }
     }
+
+    //Alternative way of writing it: using hasOwnProperty
+    // const getValue = function (key) {
+    //     if (secretVault.hasOwnProperty(key)) {
+    //         return secretVault[key];
+    //     } else {
+    //         return null;
+    //     }
+    // }
+
+    //A third way of writing it 
+    // const getValue = function (key) {
+    //     if (key in secretVault) {
+    //         return secretVault[key];
+    //     } else {
+    //         return null;
+    //     }
+    // }
+
 
     return {
         getValue: getValue,
