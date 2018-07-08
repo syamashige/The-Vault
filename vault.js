bc2d8995befdee15927242553af0977da9b33270
Sarah <yamashige.sarah@gmail.com>
2018-07-07 15:57:57 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
-  
+    const secretVault = {};
+    const setValue = function (key, value) {
+        secretVault[key] = value;
+    }
+    const getValue = function (key) {
+        if (secretVault[key] === undefined) {
+            return null;
+         } else {
+            return secretVault[key];
+        }
+    }
+
+    return {
+        getValue: getValue,
+        setValue: setValue
+    }
+
 };
